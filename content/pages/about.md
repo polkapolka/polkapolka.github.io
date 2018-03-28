e84f1188c6dd1508efb7900a6c04a61c936b013e
Phebe Polk <phebepolk@gmail.com>
2018-03-28 16:57:41 -0700

    v2

--- content/pages/about.md
+++ content/pages/about.md
@@ -1,3 +1,6 @@
 Title: About Me
 
 Hello, My name is Phebe and I am working on a portfolio website.
+
+
+[comment]: #![logo] ({filename}/images/LGBT_Rainbow_Flag.png "Picture")
--- pelicanconf.py
+++ pelicanconf.py
@@ -6,7 +6,7 @@ from __future__ import unicode_literals
 
 
 AUTHOR = u'Phebe Polk'
-SITENAME = u'Phebe Polk'
+SITENAME = u'Polka'
 SITEURL = ''
 
 PATH = 'content'
@@ -16,9 +16,9 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
 
-# USE_FOLDER_AS_CATEGORY = True
-# DEFAULT_DATE_FORMAT = '%a %d %B %Y'
-# DEFAULT_DATE = 'fs'
+USE_FOLDER_AS_CATEGORY = True
+DEFAULT_DATE_FORMAT = '%a %d %B %Y'
+DEFAULT_DATE = 'fs'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -36,12 +36,15 @@ SOCIAL = (('LinkedIn', 'https://www.linkedin.com/in/phebepolk/','linkedin'),
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
 
-# ARTICLE_URL = '{category}/{slug}/'
-# ARTICLE_SAVE_AS = '{category}/{slug}/index.html'
-# PAGE_URL = '{slug}.html'
-# PAGE_SAVE_AS = '{slug}.html'
+ARTICLE_URL = '{category}/{slug}.html'
+ARTICLE_SAVE_AS = '{category}/{slug}.html'
+PAGE_URL = '{slug}.html'
+PAGE_SAVE_AS = '{slug}.html'
+
+HIDE_SIDEBAR = True
+
 # TAG_URL = 'tags/{slug}.html'
 # TAG_SAVE_AS = 'tags/{slug}.html'
 # TAGS_URL = 'tags.html'
@@ -49,11 +52,51 @@ DEFAULT_PAGINATION = False
 ## Generate archive
 #YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
 
-ABOUT_ME = "Words Go Here."
+
+# # works with chameleon theme
+# MENUITEMS = [
+#     ('Home', '/'),
+#     ('Contact', [('About Me','category/pages/about.html'),('LinkedIn', 'https://www.linkedin.com/in/phebepolk/'),
+#           ('GitHub', 'https://github.com/polkapolka'),
+#           ('Stack Overflow', 'https://stackoverflow.com/users/4165272/polka')]),
+#     ]
+
+
+DISPLAY_PAGES_ON_MENU = False
+
+SITELOGO = 'images/favicon.ico'
+SITELOGO_SIZE = '24'
+
+HIDE_SITENAME = False
+
+GITHUB_REPO_COUNT = 2
+
+ADDTHIS_FACEBOOK_LIKE = False
+ADDTHIS_GOOGLE_PLUSONE = False
+ADDTHIS_TWEET = False
+
+#ABOUT_ME = "Words Go Here."
 
 JINJA_ENVIRONMENT = {
     'extensions': ['jinja2.ext.i18n'],
 }
+
+
+# #BOOTSTRAP_NAVBAR_INVERSE = True ## for inversing colors at navbar
+# GITHUB_USER = 'polkapolka'  ## just put your github username here.
+# GITHUB_SKIP_FORK = True
+
+FOOTER_INSERT_HTML = '<div class="footer-social"><span>Connect with us:</span><li><a href="https://www.linkedin.com/in/phebepolk/"><img src="linkedin" /></a></li><li><a href="https://github.com/polkapolka"><img src="github" /></a></li><li><a href="https://stackoverflow.com/users/4165272/polka"><img src="stack-overflow" /></a></li>'
+
+
+#DISPLAY_CATEGORIES_ON_SIDEBAR = True
+
+STATIC_PATHS = ['images']
+
+SHOW_SERIES = True
+
+DISPLAY_ARTICLE_INFO_ON_INDEX = False
+SHOW_FOOTER=False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
@@ -62,16 +105,14 @@ MARKUP = ('md', 'ipynb')
 
 PYGMENTS_STYLE = 'monokai'
 
-BOOTSTRAP_THEME = "Flatly"
+BOOTSTRAP_THEME = "flatly"
 
 PLUGIN_PATHS = ['./plugins','./pelican-plugins']
-PLUGINS = ['ipynb.markup','i18n_subsites','autopages']
+PLUGINS = ['ipynb.markup','i18n_subsites','simple_footnotes', 'extract_toc','series','footer_insert']
 #,'simple_footnotes', 'extract_toc', 'feed_summary','sitemap',
 THEME = "./pelican-themes/pelican-bootstrap3"
 
-# AUTHOR_PAGE_PATH = './content/authors/author.md'
 
-# FEED_USE_SUMMARY = True
-# SUMMARY_MAX_LENGTH = 100
+FEED_USE_SUMMARY = True
+SUMMARY_MAX_LENGTH = 100
 
-# MD_EXTENSIONS = ['toc', 'fenced_code', 'codehilite(css_class=highlight)', 'extra']
